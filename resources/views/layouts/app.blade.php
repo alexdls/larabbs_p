8949442f610217cf919f3b87d771b58ed6826d45
alex <alex_dls@163.com>
2018-04-13 16:35:42 +0800

    WYSIWYG Editor

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
 
     {{--Styles--}}
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    @yield('styles')
 </head>
 <body>
     <div id="app" class="{{route_class()}}-page">
@@ -24,6 +25,7 @@
     </div>
 {{--Scripts--}}
     <script src="{{ asset('js/app.js') }}"></script>
+    @yield('scripts')
 
 </body>
 </html>
